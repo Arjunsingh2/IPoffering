a4a535d6a09db212c98831761e8f70fe98e3372b
Arjunsingh2 <53118674+Arjunsingh2@users.noreply.github.com>
2021-07-13 02:47:01 +0530

    Add files via upload

---
+++ Migrations/20210712135413_miglocal.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IPTreatmentOffering.Migrations
+{
+    public partial class miglocal : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Specialists",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    AreaOfExpertise = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Designation = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Experience = table.Column<int>(type: "int", nullable: false),
+                    ContactNumber = table.Column<long>(type: "bigint", nullable: false),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Specialists", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TreatmentPackages",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Ailment = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Package = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TestDetail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Cost = table.Column<int>(type: "int", nullable: false),
+                    duration = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TreatmentPackages", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Specialists");
+
+            migrationBuilder.DropTable(
+                name: "TreatmentPackages");
+        }
+    }
+}
---
+++ Migrations/20210712210643_migremote.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IPTreatmentOffering.Migrations
+{
+    public partial class migremote : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
